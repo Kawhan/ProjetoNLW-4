c3d25a0dea5f4423519c8ac9aa8d728c7d0bdcca
Kawhan Laurindo de Lima <kawhanlaurindo@Outlook.com>
2021-02-26 22:33:36 -0300

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
+
+    return(
+        <div className={ styles.profileContainer }>
+            <img src="https://github.com/Kawhan.png" alt="Kawhan Laurindo"/>
+            <div>
+                <strong>Kawhan Laurindo</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level {level}
+                </p>
+            </div>
+        </div>
+        
+    );
+}
